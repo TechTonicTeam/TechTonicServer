7d33ee119fdde1d9e56900be9f3a9ec3d34b5328
N3WERS <space.galaxy@list.ru>
2024-03-13 22:19:18 +0300

    added sorting post function

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,6 +5,6 @@ const postController = require('../controller/post.controller')
 router.post('/post', postController.createPost)
 router.put('/post/like', postController.incrementLike)
 router.put('/post/dislike', postController.decrementLike)
-router.get('/post:user_id', postController.getPost)
+router.get('/post', postController.getPost)
 
 module.exports = router
